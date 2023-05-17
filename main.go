37414bf5da2915ed3cb741ea75f5a7646b9500d8
Maxwell Alvord <maxwellalvord@gmail.com>
2023-05-17 12:36:46 -0700

    Update: write error func on failed GET.

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type apiError struct {
+	err    string
+	status int
+}
+
+func (e apiError) Error() string {
+	return e.err
+}
 
 func main() {
 	http.HandleFunc("/user", handleGetUserByID)
 	http.ListenAndServe(":3000", nil)
 }
 
-func handleGetUserByID(w http.ResponseWriter, r *http.Request) {
+func handleGetUserByID(w http.ResponseWriter, r *http.Request) error {
+	if r.Method != http.MethodGet {
+		return writeJSON(w, http.StatusMethodNotAllowed, apiError{err: "invalid method", status: http.StatusMethodNotAllowed})
 
+	}
+}
+
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
 }
